8d32ed4dad5197416f9eecad2342ecdc2fba4402
mikramanival <morbid_rythemist@yahoo.com>
2012-07-20 10:58:34 +0800

    Clean Project

---
+++ Organizer/gen/doctor/organizer/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package doctor.organizer;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Organizer/src/doctor/organizer/others/Patient.java
@@ -0,0 +1,5 @@
+package doctor.organizer.others;
+
+public class Patient {
+
+}
